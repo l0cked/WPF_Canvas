0fe342424703c7e9444069cb2720417308b6c715
l0cked <l0cked1981@gmail.com>
2022-10-10 15:52:18 +0300

    Base

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace WPF_Canvas
+﻿namespace WPF_Canvas
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    public partial class App { }
 }
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WPF_Canvas.View
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
